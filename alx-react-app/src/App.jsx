93c8b6b149b97a11ada22aa274f59c6ad1a1af77
Recheal Gademeh <rechealgademehetornam@gmail.com>
2024-11-03 15:42:24 +0000

    add new components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,6 @@
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 import WelcomeMessage from './components/WelcomeMessage';
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
@@ -9,6 +12,9 @@ function App() {
 
   return (
     <>
+    <Header/>
+    <MainContent/>
+    <Footer/>
       <WelcomeMessage />
       <div>
         <a href="https://vite.dev" target="_blank" rel="noopener noreferrer">
