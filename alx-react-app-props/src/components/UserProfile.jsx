719439d471573724528ffe2ed3c5ac63ffd0d51f
Recheal Gademeh <rechealgademehetornam@gmail.com>
2024-11-10 18:55:55 +0000

    Add new props

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -5,9 +5,9 @@ const UserProfile = () => {
   const userData = UserContext(useContext)
     return (
       <div>
-        <h2>{props.name}</h2>
-        <p>Age: {props.age}</p>
-        <p>Bio: {props.bio}</p>
+        <h2>{userData.name}</h2>
+        <p>Age: {userData.age}</p>
+        <p>Bio: {userData.bio}</p>
       </div>
     );
   };
