e63113a2f67acf0bbe6a4316cd0557725419d4b5
Recheal Gademeh <rechealgademehetornam@gmail.com>
2024-11-16 14:03:37 +0000

    recipe app

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import AddRecipeForm from './components/AddRecipeForm,jsx/index.js'
+import AddRecipeForm from './components/AddRecipeForm.jsx/index.js'
 import './App.css'
 import RecipeList from './components/RecipeList.jsx'
 
