326e1f2ed7d6e0ec7991676745a4dcf176312b3f
Recheal Gademeh <rechealgademehetornam@gmail.com>
2024-11-16 15:12:40 +0000

    recipe app

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,20 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import AddRecipeForm from './components/AddRecipeForm.jsx/index.js'
-import './App.css'
-import RecipeList from './components/RecipeList.jsx'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import AddRecipeForm from "./components/AddRecipeForm.jsx";
+import "./App.css";
+import RecipeList from "./components/RecipeList.jsx";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    <h1>Recipe Sharing App</h1>
-<AddRecipeForm />
-<RecipeList />
+      <h1>Recipe Sharing App</h1>
+      <AddRecipeForm />
+      <RecipeList />
+      {/* <useRecipeStore /> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,9 +1,8 @@
-import create from 'zustand'
+import { create } from 'zustand';
 
 const useRecipeStore = create(set => ({
   recipes: [],
   addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
   setRecipes: (recipes) => set({ recipes })
 }));
-
 export default useRecipeStore
