e23736ea9e44cb51ae2ffdb137a2dd4a6973c2d2
Recheal Gademeh <rechealgademehetornam@gmail.com>
2024-11-24 22:48:47 +0000

    first commit

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import RegistrationForm from './components/RegistrationForm.jsx'
-import formikForm from './components/formikForm.jsx'
+import formikForm from './components/formikForm.js'
 
 const queryClient = new QueryClient();
 function App() {
