068896cc2199773c15b1054a2679febe0c2e3c41
Recheal Gademeh <rechealgademehetornam@gmail.com>
2024-12-05 15:13:34 +0000

    add tailwindto css

---
+++ tailwind-react-integration/tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: { extend: {} },
+  variants: { extend: {} },
+  plugins: [],
+};
